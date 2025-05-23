6c08ff31088976abf6414e1f7d632496e681b6b2
vishi saxena <saxenavishi444@gmail.com>
2025-05-23 23:45:36 +0530

    urls

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from . import views  # Import your views file
+from django.urls import path,include
+from ..core import views  # Import your views file
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('', include('core.urls')), 
 ]
